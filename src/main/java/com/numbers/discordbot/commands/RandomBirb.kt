04dc70bb0a6e2156d90e0f5206b06f3abc303d7c
Bart Arys <mysterybtdn@gmail.com>
2017-10-11 14:36:43 +0200

    added random Bird picture command

---
+++ src/main/java/com/numbers/discordbot/commands/RandomBirb.kt
@@ -0,0 +1,27 @@
+package com.numbers.discordbot.commands
+
+import com.numbers.discordbot.Command
+import com.numbers.discordbot.commands.util.withDefaultColor
+import com.numbers.discordbot.filter.MessageFilter
+import com.numbers.jttp.Jttp
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageEvent
+import sx.blah.discord.util.EmbedBuilder
+
+@Command(name = "Random birb")
+class RandomBirb{
+
+    @Command
+    @MessageFilter(eventType = MessageEvent::class, prefixCheck = true, readableUsage = "birb", regex = "birb")
+    fun randomBirb(event: MessageEvent, jttp : Jttp){
+        tweet(event, jttp)
+    }
+
+    fun tweet(event: MessageEvent, jttp: Jttp){
+        val url = jttp.get("http://random.birb.pw/tweet/").asString().join().response
+
+        println(url)
+        val embed = EmbedBuilder().withDefaultColor().withImage("http://random.birb.pw/img/$url").build()
+        event.channel.sendMessage(embed)
+    }
+
+}
