6c30fd9012d75ed1b7f1ad159d45f6c873eb010b
Bart Arys <mysterybtdn@gmail.com>
2018-02-14 13:33:01 +0100

    moved /kotlin to /java
    blame intellij

--- src/main/java/com/numbers/discordbot/network/responses.kt
+++ src/main/java/com/numbers/discordbot/network/responses.kt
@@ -4,6 +4,4 @@ data class EightBallResponse(val question: String, val answer: String, val type:
 
 data class DiscordEmojiResponse(val id: String, val title: String, val slug: String, val description: String, val category: Long, val submittedBy: String)
 
-fun DiscordEmojiResponse.slugUrl(): String {
-    return "https://discordemoji.com/assets/emoji/${this.slug}.png"
-}
+val DiscordEmojiResponse.slugUrl: String get() = "https://discordemoji.com/assets/emoji/${this.slug}.png"
