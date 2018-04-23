5b17394de138c4e5a2c78b8336f236a3273cf0b1
BartArys <mysterybtdn@gmail.com>
2018-04-23 21:25:17 +0200

    migrated DisKo to other repository
    https://github.com/BartArys/DisKo

--- DisKo/src/main/kotlin/com/numbers/disko/discord/extensions/VoiceChannelExtensions.kt
+++
@@ -1,5 +0,0 @@
-package com.numbers.disko.discord.extensions
-
-import sx.blah.discord.handle.obj.IVoiceChannel
-
-inline val IVoiceChannel.isFull: Boolean get() = userLimit > 0 && connectedUsers.count() >= userLimit
